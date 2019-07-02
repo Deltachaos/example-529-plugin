129f606bded1a6b3f52036fd7056d3de71497581
Maximilian Ruta <mr@xtain.net>
2019-07-02 11:22:35 +0200

    fix

--- src/Plugin.php
+++ src/Plugin.php
@@ -2,12 +2,18 @@
 
 namespace Deltachaos\Example529;
 
+use Composer\Composer;
+use Composer\IO\IOInterface;
 use Composer\Plugin\PluginInterface;
 use Composer\EventDispatcher\EventSubscriberInterface;
 use Composer\Script\ScriptEvents;
 
 class Plugin implements PluginInterface, EventSubscriberInterface
 {
+    public function activate(Composer $composer, IOInterface $io)
+    {
+    }
+
     public function createProject()
     {
         echo "hello world\n";
